87f3e0b52d553b8389c6a35934adda712bbe7f93
Pravin Gupta <pravingupta1071@gmail.com>
2019-08-12 09:04:49 +0530

    Add Private Routes

---
+++ src/components/LoginPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export class LoginPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<button onClick = {this.props.startLogin}>Login</button>
+			</div>
+		);
+	};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+	startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
