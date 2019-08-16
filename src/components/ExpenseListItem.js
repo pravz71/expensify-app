01e6a0edb93679c1a08208999c7bbd199b14b726
Pravin Gupta <pravingupta1071@gmail.com>
2019-08-16 23:31:54 +0530

    styling and final deployment

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,16 +4,14 @@ import moment from 'moment';
 import numeral from 'numeral';
 
 const ExpenseListItem = (props) => (
-	<div>
+	<Link className="list-item" to={`/edit/${props.id}`}>
 		<div>
-			<Link to={`/edit/${props.id}`}>
-				<h3>{props.description}</h3>
-			</Link>
+			<h3 className="list-item__title">{props.description}</h3>
+			<span className="list-item__subtitle">{moment(props.createdAt).format('MMMM Do, YYYY')}</span>
+			<p className="list-item__optional">{ (props.note) ? 'Note: ' + props.note  :  'Add a note' }</p>
 		</div>
-		<p>
-			{'Rs.' + numeral(props.amount).format('0,0[.]00')} - {moment(props.createdAt).format('MMMM Do, YYYY')}
-		</p>
-	</div>
+		<h3 className="list-item__data"> {'Rs.' + numeral(props.amount).format('0,0[.]00')} </h3>
+	</Link>
 );
 
 export default ExpenseListItem;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,12 @@ import { startLogin } from '../actions/auth';
 export class LoginPage extends React.Component {
 	render() {
 		return (
-			<div>
-				<button onClick = {this.props.startLogin}>Login</button>
+			<div className="box-layout">
+				<div className="box-layout__box">
+					<h1 className="box-layout__title">Expensify</h1>
+					<p>It's time to get your expenses under control.</p>
+					<button onClick = {this.props.startLogin} className="button">Login with Google</button>
+				</div>
 			</div>
 		);
 	};
